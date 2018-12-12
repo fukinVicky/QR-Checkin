4a517e0100b28c7702879c342ccb0cf42fac651f
Cynthia Stephany Rivera Bacelis <csriverab@concredito.com.mx>
2018-12-12 09:06:41 -0700

    scan

--- QR-Checkin/src/components/Scanner/Scanner.js
+++ QR-Checkin/src/components/Scanner/Scanner.js
@@ -2,40 +2,48 @@ import React, { Component } from "react";
 import QrReader from "react-qr-reader";
 
 class Scanner extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      delay: 300,
-      result: "No result"
-    };
-    this.handleScan = this.handleScan.bind(this);
-  }
-  handleScan(data) {
-    if (data) {
-      this.setState({
-        result: data
-      });
-
-      console.log(data);
-      
+    constructor(props) {
+        super(props);
+        this.state = {
+            delay: 300,
+            result: "No result",
+        };
     }
-  }
-  handleError(err) {
-    console.error(err);
-  }
-  render() {
-    return (
-      <div>
-        <QrReader
-          delay={this.state.delay}
-          onError={this.handleError}
-          onScan={this.handleScan}
-          style={{ width: "50%" }}
-        />
-        <p>{this.state.result}</p>
-      </div>
-    );
-  }
+
+    handleScan = (data) => {
+        if (data) {
+            this.setState({
+                result: data,
+            });
+
+            console.log(data);
+
+        }
+    }
+
+    handleError = (err) => {
+        console.error(err);
+    }
+
+    render() {
+
+        let style = {
+            'background': 'white',
+            width: '100%',
+        }
+
+        return (
+            <div style={style}>
+                <QrReader
+                    delay={this.state.delay}
+                    onError={this.handleError}
+                    onScan={this.handleScan}
+                    style={{ width: "50%" }}
+                />
+                <p >{this.state.result}</p>
+            </div>
+        );
+    }
 }
 
 export default Scanner;
